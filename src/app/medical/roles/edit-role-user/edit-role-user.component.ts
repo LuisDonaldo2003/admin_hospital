c52eaf8d796e2e2e81507bfff9470769b4e7e446
LuisDonaldo2003 <l21930214@cdaltamirano.tecnm.mx>
2025-03-26 23:02:44 -0600

    UpdatedEditRoleUser

--- src/app/medical/roles/edit-role-user/edit-role-user.component.ts
+++ src/app/medical/roles/edit-role-user/edit-role-user.component.ts
@@ -1,11 +1,85 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RolesService } from '../service/roles.service';
+import { DataService } from 'src/app/shared/data/data.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-edit-role-user',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule,CommonModule],
   templateUrl: './edit-role-user.component.html',
   styleUrl: './edit-role-user.component.scss'
 })
 export class EditRoleUserComponent {
+
+  sideBar:any = [];
+  name:string = '';
+  permissions:any = [];
+  valid_form: boolean = false;
+  valid_form_success: boolean = false;
+  text_validation:any = null;
+
+  role_id:any;
+  constructor(
+    public DataService: DataService ,
+    public RoleService: RolesService,
+    public activedRoute: ActivatedRoute,
+  ) {
+
+  }
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.sideBar = this.DataService.sideBar[0].menu;
+    this.activedRoute.params.subscribe((resp:any) => {
+      this.role_id = resp.id;
+    })
+    this.showRole();
+  }
+
+  showRole() {
+    this.RoleService.showRoles(this.role_id).subscribe((resp: any) => {
+      console.log(resp);
+      this.name = resp.name;
+      this.permissions = Array.isArray(resp.permission_pluck) ? resp.permission_pluck : [];
+    });
+  }
+
+
+  addPermission(subMenu:any){
+    if(subMenu.permision){
+      let INDEX = this.permissions.findIndex((item:any) => item == subMenu.permision);
+      if(INDEX != -1){
+        this.permissions.splice(INDEX,1);
+      }else{
+        this.permissions.push(subMenu.permision);
+      }
+      console.log(this.permissions);
+    }
+  }
+
+  save(){
+    this.valid_form = false;
+    if(!this.name  || this.permissions.length == 0){
+      this.valid_form = true;
+      return;
+    }
+    let data = {
+      name: this.name,
+      permisions:this.permissions,
+    };
+    this.valid_form_success = false;
+    this.text_validation = null;
+    this.RoleService.editRoles(data,this.role_id).subscribe((resp:any) => {
+      console.log(resp);
+      if(resp.message == 403){
+        this.text_validation = resp.message_text;
+        return ;
+      }
+      this.valid_form_success = true;
+    })
+  }
 
 }
