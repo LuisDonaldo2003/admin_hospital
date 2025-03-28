e84f871f1d3e4b7d7acbcb33e9fb7da883dfbd99
LuisDonaldo2003 <l21930214@cdaltamirano.tecnm.mx>
2025-03-27 20:46:16 -0600

    UpdateCleanSesionUsers

--- src/app/common-component/sidebar/sidebar.component.ts
+++ src/app/common-component/sidebar/sidebar.component.ts
@@ -1,39 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/shared/auth/auth.service';
 import { DataService } from 'src/app/shared/data/data.service';
 import { MenuItem, SideBarData } from 'src/app/shared/models/models';
 import { routes } from 'src/app/shared/routes/routes';
 import { SideBarService } from 'src/app/shared/side-bar/side-bar.service';
 
 @Component({
-    selector: 'app-sidebar',
-    templateUrl: './sidebar.component.html',
-    styleUrls: ['./sidebar.component.scss'],
-    standalone: false
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss'],
+  standalone: false
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnDestroy {
   base = '';
   page = '';
   currentUrl = '';
   public classAdd = false;
-
   public multilevel: Array<boolean> = [false, false, false];
 
   public routes = routes;
   public sidebarData: Array<SideBarData> = [];
+  public user: any;
+
+  private userSubscription: Subscription;
 
   constructor(
     private data: DataService,
     private router: Router,
-    private sideBar: SideBarService
+    private sideBar: SideBarService,
+    public authService: AuthService,
   ) {
-    this.sidebarData = this.data.sideBar;
+    this.userSubscription = this.authService.user$.subscribe(user => {
+      this.user = user;
+      this.loadSidebar();
+    });
+
     router.events.subscribe((event: object) => {
       if (event instanceof NavigationEnd) {
         this.getRoutes(event);
       }
     });
     this.getRoutes(this.router);
+  }
+
+  private loadSidebar() {
+    if (!this.user) {
+      this.sidebarData = [];
+      return;
+    }
+
+    if (this.user.roles.includes("Super-Admin")) {
+      this.sidebarData = this.data.sideBar;
+    } else {
+      let permissions = this.user.permissions;
+      let SIDE_BAR_G: any = [];
+
+      this.data.sideBar.forEach((side: any) => {
+        let SIDE_B: any = [];
+        side.menu.forEach((menu_s: any) => {
+          if (menu_s.subMenus.length > 0) {
+            let SUB_MENUS = menu_s.subMenus.filter((submenu: any) => permissions.includes(submenu.permision) && submenu.show_nav);
+            if (SUB_MENUS.length > 0) {
+              menu_s.subMenus = SUB_MENUS;
+              SIDE_B.push(menu_s);
+            }
+          } else {
+            if (permissions.includes(menu_s.permision)) {
+              menu_s.subMenus = [];
+              SIDE_B.push(menu_s);
+            }
+          }
+        });
+        if (SIDE_B.length > 0) {
+          side.menu = SIDE_B;
+          SIDE_BAR_G.push(side);
+        }
+      });
+      this.sidebarData = SIDE_BAR_G;
+    }
   }
 
   public expandSubMenus(menu: MenuItem): void {
@@ -48,19 +94,16 @@ export class SidebarComponent {
       });
     });
   }
+
   private getRoutes(route: { url: string }): void {
     const bodyTag = document.body;
-
-    bodyTag.classList.remove('slide-nav')
-    bodyTag.classList.remove('opened')
+    bodyTag.classList.remove('slide-nav', 'opened');
     this.currentUrl = route.url;
-
     const splitVal = route.url.split('/');
-
-
     this.base = splitVal[1];
     this.page = splitVal[2];
   }
+
   public miniSideBarMouseHover(position: string): void {
     if (position == 'over') {
       this.sideBar.expandSideBar.next("true");
@@ -69,4 +112,7 @@ export class SidebarComponent {
     }
   }
 
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+  }
 }
--- src/app/shared/auth/auth.service.ts
+++ src/app/shared/auth/auth.service.ts
@@ -1,66 +1,71 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-// import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, catchError, map, of } from 'rxjs';
 import { routes } from '../routes/routes';
 import { HttpClient } from '@angular/common/http';
 import { URL_SERVICIOS } from 'src/app/config/config';
-import { catchError, map, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
 
-  user:any;
-  token:any;
-  constructor(
-    private router: Router,
-    public http: HttpClient,
-  ) {
+  private userSubject = new BehaviorSubject<any>(null);
+  user$ = this.userSubject.asObservable();
+
+  user: any;
+  token: any;
+
+  constructor(private router: Router, public http: HttpClient) {
     this.getLocalStorage();
   }
 
-  getLocalStorage(){
-    if(localStorage.getItem("token") && localStorage.getItem("user")){
-      let USER = localStorage.getItem("user");
+  getLocalStorage() {
+    if (localStorage.getItem("token") && localStorage.getItem("user")) {
+      let USER = localStorage.getItem('user');
       this.user = JSON.parse(USER ? USER : '');
       this.token = localStorage.getItem("token");
-    }else{
+      this.userSubject.next(this.user); // Notificar cambios
+    } else {
       this.user = null;
       this.token = null;
+      this.userSubject.next(null);
     }
   }
 
-  login(email:string,password:string) {
-    // Implementa la lógica de autenticación aquí.
-    let URL = URL_SERVICIOS+"/auth/login";
-    return this.http.post(URL,{email: email,password: password}).pipe(
-      map((auth:any) => {
+  login(email: string, password: string) {
+    let URL = URL_SERVICIOS + "/auth/login";
+    return this.http.post(URL, { email: email, password: password }).pipe(
+      map((auth: any) => {
         console.log(auth);
-        const result = this.saveLocalStorage(auth);
+        const result = this.savelocalStorage(auth);
         return result;
       }),
-      catchError((error:any) => {
-        console.log(error);
+      catchError((error: any) => {
+        console.error(error);
         return of(undefined);
       })
     );
   }
 
-  saveLocalStorage(auth:any){
-    if(auth && auth.access_token){
-      localStorage.setItem("token",auth.access_token);
-      localStorage.setItem("user",JSON.stringify(auth.user));
+  savelocalStorage(auth: any) {
+    if (auth && auth.access_token) {
+      localStorage.setItem("token", auth.access_token);
+      localStorage.setItem("user", JSON.stringify(auth.user));
       localStorage.setItem('authenticated', 'true');
+      this.userSubject.next(auth.user); // Notificar cambios
       return true;
     }
     return false;
   }
 
-  logout(){
+  logout() {
     localStorage.removeItem("token");
     localStorage.removeItem("user");
     localStorage.removeItem('authenticated');
-    this.router.navigate([routes.login]);
+    this.userSubject.next(null); // Notificar que el usuario se ha eliminado
+    this.router.navigate([routes.login]).then(() => {
+      window.location.reload(); // 🔴 FORZAR RECARGA TOTAL DE LA APP
+    });
   }
 }
