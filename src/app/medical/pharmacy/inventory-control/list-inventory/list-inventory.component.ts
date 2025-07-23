7740c4d348d6fb0b54dd172b3c0302bc0e71956c
LuisDonaldo2003 <l21930214@cdaltamirano.tecnm.mx>
2025-07-23 11:18:30 -0600

    UpdatePharmacy

---
+++ src/app/medical/pharmacy/inventory-control/list-inventory/list-inventory.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-inventory',
+  imports: [],
+  templateUrl: './list-inventory.component.html',
+  styleUrl: './list-inventory.component.scss'
+})
+export class ListInventoryComponent {
+
+}
